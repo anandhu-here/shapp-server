e3f4848eeaf1aad0c02aeca23ecdaf6816a3c288
Anandhu Satheesh <anandhusatheesh@Anandhus-MacBook-Air.local>
2023-09-19 18:40:23 +0100

    initial

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ io.on('connection', (socket) => {
             if(ref_loc){
                 const users_ = Object.keys(userSockets).filter((username) => {
                     const userLocation = userSockets[username].location;
-                    console.log(userSockets[username].location, "location")
+                    console.log(userSockets[username].location, "location", username)
                     const distance = calculateDistance(
                         ref_loc.latitude,
                         ref_loc.longitude,
