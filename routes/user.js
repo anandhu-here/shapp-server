86a75a40e72e54b51f607993224e838d2ee5db95
Anandhu Satheesh <anandhusatheesh@Anandhus-MacBook-Air.local>
2023-09-22 17:30:42 +0100

    init

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,21 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
+
+function calculateDistance(lat1, lon1, lat2, lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d;
+}
+
+
 // Create a new user
 router.post('/', async (req, res) => {
   try {
@@ -18,7 +33,17 @@ router.get('/getUsers/:username', async(req, res)=>{
   try{
     const username = req.params.username;
     const users = await User.find({username: {$ne:username}});
-    res.status(200).json({users:users})
+    const curUser = await User.findOne({username:username});
+    let users_ = [];
+
+    users.map(user=>{
+      const distance = calculateDistance( curUser.location.latitude, curUser.location.longitude, user.location.latitude, user.location.longitude )
+      if(distance <= mile && user.username!==username ){
+          users_.unshift({username: user.username, distance:distance});
+      }
+    })
+    
+    res.status(200).json({users:users_})
   }
   catch(error){
     res.status(400).json({ error: error.message });
